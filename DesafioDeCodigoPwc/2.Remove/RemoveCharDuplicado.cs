ac7eb54afbde714299a891b4ad68b6f98f372115
renatajane <renatajane28@gmail.com>
2023-07-08 11:55:28 -0300

    Remover caracteres duplicados

---
+++ DesafioDeCodigoPwc/2.Remove/RemoveCharDuplicado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigoPwc._2.Remove
+{
+    internal class RemoveCharDuplicado
+    {
+        public string RemoverCaracteresDuplicados(string frase)
+        {
+
+            string fraseSemRepeticao = string.Join("", frase.ToCharArray().Distinct());
+
+            return fraseSemRepeticao;
+        }
+    }
+}
--- DesafioDeCodigoPwc/Program.cs
+++ DesafioDeCodigoPwc/Program.cs
@@ -2,3 +2,6 @@
 var reverte = new DesafioDeCodigoPwc._1.Reverter.ReverteOrdem();
 Console.WriteLine(reverte.ReverterFrase("Hello, World! OpenAI is amazing."));
 
+var remove = new DesafioDeCodigoPwc._2.Remove.RemoveCharDuplicado();
+Console.WriteLine(remove.RemoverCaracteresDuplicados("Hello, World!")); 
+
