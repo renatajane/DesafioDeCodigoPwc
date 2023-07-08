cb79726f58588e2f5d7095a227f4e1c54825d3f4
renatajane <renatajane28@gmail.com>
2023-07-08 12:02:16 -0300

    Identificar o palindromo mais longo

---
+++ DesafioDeCodigoPwc/3.PalindromoLongo/PalindromoMaisLongo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigoPwc._3.PalindromoLongo
+{
+    internal class PalindromoMaisLongo
+    {
+        public string IdentificarPalindromoMaisLongo(String palavra)
+        {
+            int maxLength = 1;
+            int start = 0;
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                for (int j = i; j < palavra.Length; j++)
+                {
+                    bool isPalindrome = true;
+
+                    for (int k = 0; k < (j - i + 1) / 2; k++)
+                    {
+                        if (palavra[i + k] != palavra[j - k])
+                        {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+
+                    if (isPalindrome && (j - i + 1) > maxLength)
+                    {
+                        start = i;
+                        maxLength = j - i + 1;
+                    }
+                }
+            }
+            return palavra.Substring(start, maxLength);
+         }
+    }
+}
--- DesafioDeCodigoPwc/Program.cs
+++ DesafioDeCodigoPwc/Program.cs
@@ -3,5 +3,7 @@ var reverte = new DesafioDeCodigoPwc._1.Reverter.ReverteOrdem();
 Console.WriteLine(reverte.ReverterFrase("Hello, World! OpenAI is amazing."));
 
 var remove = new DesafioDeCodigoPwc._2.Remove.RemoveCharDuplicado();
-Console.WriteLine(remove.RemoverCaracteresDuplicados("Hello, World!")); 
+Console.WriteLine(remove.RemoverCaracteresDuplicados("Hello, World!"));
 
+var identifica = new DesafioDeCodigoPwc._3.PalindromoLongo.PalindromoMaisLongo();
+Console.WriteLine(identifica.IdentificarPalindromoMaisLongo("babad"));
