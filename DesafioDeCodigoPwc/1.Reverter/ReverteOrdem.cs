1595bc6623c570becf09336d0e08fe52e9508ad4
renatajane <renatajane28@gmail.com>
2023-07-08 11:48:02 -0300

    Reverte ordem das palavras

---
+++ DesafioDeCodigoPwc/1.Reverter/ReverteOrdem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigoPwc._1.Reverter
+{
+    internal class ReverteOrdem
+    {
+        public string ReverterFrase (string frase)
+        {
+            string[] palavras = frase.Split(' ');
+            Array.Reverse(palavras);
+
+            string fraseInversa = string.Join(" ", palavras);
+
+            return fraseInversa;
+        }
+    }
+}
---
+++ DesafioDeCodigoPwc/Program.cs
@@ -0,0 +1,4 @@
+﻿
+var reverte = new DesafioDeCodigoPwc._1.Reverter.ReverteOrdem();
+Console.WriteLine(reverte.ReverterFrase("Hello, World! OpenAI is amazing."));
+
